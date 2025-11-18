1ab3cad9d407d00ce4a60f0d617d2faf67478f96
jmalonso-mit <65486960+jmalonso-mit@users.noreply.github.com>
2025-11-18 12:35:23 +0100

    Fix print statement in on_ready event

--- discord_only.py
+++ discord_only.py
@@ -13,8 +13,7 @@ client = discord. Client(intents=intents)
 
 @client.event
 async def on_ready():
-print('We have logged in as {0.user}'.format(client
-
+print(f"Bot conectado como {discord_client.user}")
 @client.event
 async def on_message(message):
 if message.author == client.user:
